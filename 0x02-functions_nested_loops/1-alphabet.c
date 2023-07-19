fa3fbac396ba6fddf8f5ecdc06d77a59cb2f6b90
mooreArrqs <amandanxumalo921@gmail.com>
2023-07-19 04:53:18 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Generates the alphabet
+ *
+ * Return: Void
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+_putchar(ch);
+_putchar('\n');
+}
