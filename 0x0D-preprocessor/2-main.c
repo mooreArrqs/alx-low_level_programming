4c687467f1dcd5a38204005b2800dda72fed7d86
mooreArrqs <amandanxumalo921@gmail.com>
2023-08-14 02:54:53 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of a program
+ * it was compiled from.
+ *
+ * Return : Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+}
