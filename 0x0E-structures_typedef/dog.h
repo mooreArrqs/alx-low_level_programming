6c9a5bf044f1c1b9226d39d821929392aa873de1
Amanda Nxumalo <amandanxumalo921@gamail.com>
2023-11-06 04:44:22 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * strct dog - A new type defining a dog.
+ *
+ * @name: The name of a dog.
+ * @age: The age of a dog.
+ * @owner: The owner of a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
